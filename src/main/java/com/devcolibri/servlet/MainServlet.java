5545b6ec8873eac784e22f4b31684b3474c777eb
Mihail Bogdanchikov <mihailBogdanchikovWork@gmail.com>
2021-12-07 17:07:21 +0300

    Просто здравствуй

--- src/main/java/com/devcolibri/servlet/MainServlet.java
+++ src/main/java/com/devcolibri/servlet/MainServlet.java
@@ -14,7 +14,7 @@ public class MainServlet  extends HttpServlet {
             throws ServletException, IOException {
 
         PrintWriter out = resp.getWriter();
-        out.print("<h1>Hello Servlet TOmcat Privet SSS</h1>");
+        out.print("<h1>Hello </h1>");
 
     }
 
